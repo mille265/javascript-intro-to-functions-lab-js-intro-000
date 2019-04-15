a01780eddab4b6a5153e152b227a4753e990962a
meghan miller <mille265@gmail.com>
2019-04-15 15:25:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,12 +15,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 
 }
-function sayHiToGrandma(string) {
+function sayHiToGrandma(whisper.string) {
   returns ("I can\'t hear you!")
 
 }
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
