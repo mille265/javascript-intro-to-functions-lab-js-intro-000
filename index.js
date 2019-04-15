0aa789c8e0bdad445cfed342987b024619a4b32e
meghan miller <mille265@gmail.com>
2019-04-15 16:01:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ function shout(string) {
 
 
 }
+function whisper(string) {
+  return string.toLowerCase()
+
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());;
+
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+
+}
