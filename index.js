79f6c3e686672fd6d6de2a307af8bed3da553c93
meghan miller <mille265@gmail.com>
2019-04-15 16:21:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ function logWhisper(string) {
 
 }
 
-function sayHiToGrandma(string){
-  var uppercase = "HELLO"
-  var lowercase = "hello"
-  if (string === lowercase){
+if (string === lowercase){
 if (string === string.toLowerCase()){
   return "I can't hear you!"
 }else if (string === uppercase){
