0b9626b29ab6007e27bb4207fe96b5f0ebcdf2dd
meghan miller <mille265@gmail.com>
2019-04-15 15:19:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(whisper) {
   return ("I can\'t hear you!")
-  
+
 }
 
 function sayHiToGrandma(shout) {
   return ("YES INDEED!")
-  
+
 }
